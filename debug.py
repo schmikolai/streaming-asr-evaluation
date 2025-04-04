35892715b1ca8eda33f976cd568eff64c5186014
Nikolai Schmidt <schmikolai@gmail.com>
2025-04-04 11:45:33 +0200

    added wer metric after denug run

--- debug.py
+++ debug.py
@@ -9,6 +9,7 @@ from src.eval.DebugTranscriberAdapter import DebugTranscriberAdapter
 from src.eval.WebsocketTranscriberAdapter import WebsocketTranscriberAdapter
 
 import asyncio
+from jiwer import wer
 
 init_logger()
 
@@ -18,7 +19,7 @@ logger = logging.getLogger("src.Main")
 dataset = Dataset()
 element = next(dataset)
 
-logger.info(f"Dataset element {element[0]} with length {len(element[1])} bytes and transcript: \"{element[2]}\"")
+logger.info(f"Evaluating dataset element {element[0]} with length {len(element[1])} bytes ")
 
 w = Whisper.for_gpu("large-v3-turbo", [0])
 adapter = Stream(w)
@@ -29,6 +30,7 @@ logger.info(f"Transcribing dataset element with ID {element[0]}")
 async def run_transcription():
     transcription = await transcriber.transcribe(element[1])
     logger.info(f"Transcription result: \"{transcription}\"")
+    transcription_wer = wer(element[2], transcription)
+    logger.info(f"Transcription WER: {transcription_wer}")
 
 asyncio.run(run_transcription())
-

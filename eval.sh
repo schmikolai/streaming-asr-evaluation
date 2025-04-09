a916e7b4c3c09b1763056a973e8b737f1836af5a
Nikolai Schmidt <schmikolai@gmail.com>
2025-04-09 13:05:56 +0200

    added node filter to slurm script

--- eval.sh
+++ eval.sh
@@ -3,6 +3,9 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=2
 #SBATCH --time=08:00:00
+#SBATCH --exclude=marvin,deepthought,glados,jarvis
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=ns115@hdm-stuttgart.de
 
 conda activate asr
 

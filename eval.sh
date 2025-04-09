80d65b28e587755230ce766f7ee1d63d8f108283
Schmidt Nikolai <ns115@deeplearn.mi.hdm-stuttgart.de>
2025-04-09 13:27:10 +0200

    optimized slurm params for eval script

--- eval.sh
+++ eval.sh
@@ -4,9 +4,14 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --time=08:00:00
 #SBATCH --exclude=marvin,deepthought,glados,jarvis
+#SBATCH --output=/path/to/logs/%x_%j.out
+#SBATCH --error=/path/to/logs/%x_%j.err
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=ns115@hdm-stuttgart.de
 
+source ~/miniconda3/etc/profile.d/conda.sh
 conda activate asr
+
+cd "$(dirname "$0")"
 
 python -u run.py

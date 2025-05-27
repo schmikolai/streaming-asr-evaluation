3670a9894327362c2043472f55ca0f8b8eca9676
Nikolai Schmidt <schmikolai@gmail.com>
2025-05-27 16:58:25 +0200

    added final_mfa control sequence

--- src/eval/SampleResult.py
+++ src/eval/SampleResult.py
@@ -7,6 +7,7 @@ from typing import Literal
 from src.eval.PredictionAlignment import PredictionAlignment
 from src.eval.metrics.word_first_correct import word_first_correct_response
 from src.eval.metrics.word_first_final import word_first_final_response
+import warnings
 
 @dataclass
 class WordResult:
@@ -39,6 +40,7 @@ class SampleResult:
     alignments: list[PredictionAlignment] = None
     transcript: str = None
     mfa: list[WordResult] = None
+    final_mfa: list[WordResult] = None
 
     _alignment_sequence: list[WordResult] = None
 
@@ -51,6 +53,7 @@ class SampleResult:
         baseline: list[WordResult] = None,
         transcript: list[WordResult] = None,
         mfa: list[WordResult] = None,
+        final_mfa: list[WordResult] = None,
     ):
         self.sample_id = sample_id
         self.final = final
@@ -59,6 +62,7 @@ class SampleResult:
         self.baseline = baseline
         self.transcript = transcript
         self.mfa = mfa
+        self.final_mfa = final_mfa
 
     @classmethod
     def load_by_id(cls, directory: str, sample_id: str):
@@ -111,31 +115,62 @@ class SampleResult:
 
         with open(os.path.join(directory, sample_id + "_partial.json"), "r") as f:
             partials_data = json.load(f)
+            # Baseline
+            baseline_path = os.path.join("../out/baseline", sample_id + ".json")
+            if os.path.exists(baseline_path):
+                with open(baseline_path, "r") as f:
+                    baseline_data = json.load(f)
+                    baseline = parse_word_result_list(baseline_data)
+            else:
+                warnings.warn(f"Baseline file not found: {baseline_path}")
+                baseline = None
 
-        with open(os.path.join("../out/baseline", sample_id + ".json"), "r") as f:
-            baseline_data = json.load(f)
-        
-        with open(os.path.join("../data/librispeech-pc-test-clean", sample_id, sample_id + ".txt"), "r") as f:
-            transcript_str = f.read().strip()
-        
-        tg = TextGrid.fromFile(os.path.join("../data/mfa", sample_id, sample_id + ".TextGrid"), "r")
-        mfa_data = tg.getFirst("words").intervals
+            # Transcript
+            transcript_path = os.path.join("../data/librispeech-pc-test-clean", sample_id, sample_id + ".txt")
+            if os.path.exists(transcript_path):
+                with open(transcript_path, "r") as f:
+                    transcript_str = f.read().strip()
+                    transcript = parse_transcript(transcript_str)
+            else:
+                warnings.warn(f"Transcript file not found: {transcript_path}")
+                transcript = None
 
-        obj = cls(
-            sample_id=sample_id,
-            final=parse_word_result_list(final_data),
-            final_messages=parse_final_messages(final_messages_data),
-            partials=parse_partials(partials_data),
-            baseline=parse_word_result_list(baseline_data),
-            transcript=parse_transcript(transcript_str),
-            mfa=parse_mfa(mfa_data),
-        )
+            # MFA
+            mfa_path = os.path.join("../data/mfa", sample_id, sample_id + ".TextGrid")
+            if os.path.exists(mfa_path):
+                tg = TextGrid.fromFile(mfa_path)
+                mfa_data = tg.getFirst("words").intervals
+                mfa = parse_mfa(mfa_data)
+            else:
+                warnings.warn(f"MFA file not found: {mfa_path}")
+                mfa = None
+
+            # Final MFA
+            final_mfa_path = os.path.join(directory, "mfa", sample_id, sample_id + ".TextGrid")
+            if os.path.exists(final_mfa_path):
+                tg = TextGrid.fromFile(final_mfa_path)
+                final_mfa_data = tg.getFirst("words").intervals
+                final_mfa = parse_mfa(final_mfa_data)
+            else:
+                warnings.warn(f"Final MFA file not found: {final_mfa_path}")
+                final_mfa = None
+
+            obj = cls(
+                sample_id=sample_id,
+                final=parse_word_result_list(final_data),
+                final_messages=parse_final_messages(final_messages_data),
+                partials=parse_partials(partials_data),
+                baseline=baseline,
+                transcript=transcript,
+                mfa=mfa,
+                final_mfa=final_mfa,
+            )
 
         return obj
 
     def build_alignments(self,
                          normalize_words=True,
-                         align_to: Literal["final", "baseline", "mfa"]="final",
+                         align_to: Literal["final", "baseline", "mfa", "final_mfa"]="final",
                          temporal_tolerance: float=0.5):
         if align_to == "baseline":
             if self.baseline is None:
@@ -147,8 +182,12 @@ class SampleResult:
             self._alignment_sequence = self.mfa
         elif align_to == "final":
             self._alignment_sequence = self.final
+        elif align_to == "final_mfa":
+            if self.final_mfa is None:
+                raise ValueError("Final MFA is not set in the sample.")
+            self._alignment_sequence = self.final_mfa
         else:
-            raise ValueError("Invalid value for align_to. Use 'final', 'mfa' or 'baseline'.")
+            raise ValueError("Invalid value for align_to. Use 'final', 'final_mfa', 'mfa' or 'baseline'.")
         
         self.alignments = []
         for timestep in range(len(self.partials)):
